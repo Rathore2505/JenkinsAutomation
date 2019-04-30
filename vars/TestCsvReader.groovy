8b4cc63737bc1d944b11bb1f40f2143c1383ef5c
Rathore2505 <49403036+Rathore2505@users.noreply.github.com>
2019-04-30 02:50:33 -0700

    Update TestCsvReader.groovy

--- vars/TestCsvReader.groovy
+++ vars/TestCsvReader.groovy
@@ -2,17 +2,19 @@
 import com.opencsv.CSVReader
 import com.opencsv.CSVWriter
 
-class TestCsvReader {
+class TestCsvReader 
+      //csv file containing data
+      String strFile = "C:\JenkinsAutomation\Newjob.csv";
+      CSVReader reader = new CSVReader(new FileReader(strFile));
+      String [] nextLine;
+      int lineNumber = 0;
+      while ((nextLine = reader.readNext()) != null) {
+        lineNumber++;
+        System.out.println("Line # " + lineNumber);
 
-
-    static main(args) {
-        def csv = '''"a","b","c"
-"d","e","f"
-'''
-        def Reader csvFileReader = new StringReader(csv)
-        def CSVReader reader = new CSVReader(csvFileReader)
-
-         println 'Values ' + reader + '.'
-        
+        // nextLine[] is an array of values from the line
+        System.out.println(nextLine[4] + "etc...");
+      }
     }
+  }
 }
