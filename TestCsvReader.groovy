6626564a23a90c1321f2527fb84334933dc3a034
vinooz-git <vinothkumar.palanisamy@ravsoftsolutions.com>
2019-04-30 02:14:54 -0700

    Code commit

---
+++ TestCsvReader.groovy
@@ -0,0 +1,20 @@
+import com.opencsv.CSVReader
+import com.opencsv.CSVWriter
+
+class TestCsvReader {
+
+
+    static main(args) {
+        def csv = '''"a","b","c"
+"d","e","f"
+'''
+        def Reader csvFileReader = new StringReader(csv)
+        def CSVReader reader = new CSVReader(csvFileReader)
+
+        reader.iterator().each { fields ->
+            writer.writeNext(fields)
+        }
+        reader.close()
+        writer.close()
+    }
+}
