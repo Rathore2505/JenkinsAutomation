05f9fdb6351591f82b71c7314b18c9d5a1192302
Rathore2505 <49403036+Rathore2505@users.noreply.github.com>
2019-05-02 03:28:43 -0700

    Update TestCsvReader.groovy

--- vars/TestCsvReader.groovy
+++ vars/TestCsvReader.groovy
@@ -3,17 +3,24 @@ import com.xlson.groovycsv.CsvParser
 
 def call()
 {
-
+    def mapList = []
+    def tmpMap = []
     File csvFile = new File('C:\\JenkinsAutomation\\Newjob.csv')
     String fileContents = new File('C:/JenkinsAutomation/Newjob.csv').text
     List<String[]> allData = fileContents.split('\n')
     for (def i=0;i<=allData.size;i++) // Read All Lines
     {
-        
-        if(allData[i].toString().contains('Emilconfig'))
+        if(allData[i].toString().contains('admin@merge.com'))
         {
-          List<String[]> Split =   allData[i].split( )
-           println '' + Split.size + '.'  
+          List<String[]> parts =   allData[i].split(,)
+             for(def j=0;j<=parts.size;j++)
+              {
+                tmpMap.putAt(parts[j])
+              }
+        }
+    }
+    mapList.add(tmpMap)
+           println '' + mapList + '.'  
         } 
         
     } 
