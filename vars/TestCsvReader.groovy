1cc3a5562dbfeacf3e946f6f577bb278865fb026
Rathore2505 <49403036+Rathore2505@users.noreply.github.com>
2019-05-02 05:48:22 -0700

    Update TestCsvReader.groovy

--- vars/TestCsvReader.groovy
+++ vars/TestCsvReader.groovy
@@ -1,7 +1,7 @@
 @Grab('com.xlson.groovycsv:groovycsv:0.2')
 import com.xlson.groovycsv.CsvParser
 
-def call()
+def TestCsvReader( int index , string Variable)
 {
     
     def list= []
@@ -10,25 +10,16 @@ def call()
     List<String[]> allData = fileContents.split('\n')
     for (def i=0;i<=allData.size;i++) // Read All Lines
     {
-        if(allData[i].toString().contains('EmailValue'))
+        if(allData[i].toString().contains('Variable'))
         {
           List<String[]> Spl = allData[i].split('=') 
-           println  ''+ Spl[0].toString()  + ''
-           println  ''+ Spl[1].toString()  + ''
-           println  ''+ allData[i].toString()  + ''
           List<String[]> parts =   Spl[1].split(',')
-             //println  ''+ parts[0].toString() + ''
-             //println  ''+ parts[1].toString() + ''
-             //println  ''+ parts[2].toString() + ''
           for(def j=0;j<parts.size;j++)
             {
             list.add(parts[j].toString()) 
             }
         }
     }
-    //mapList.add(tmpMap)
-    println ""+list[0]
-    println ""+list[1]
-    println ""+list[2]
+    return list[indix]
  
 } 
