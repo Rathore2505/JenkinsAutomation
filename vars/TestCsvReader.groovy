921ec21e567a13f3a8a097fff84e01247e772a2e
Rathore2505 <49403036+Rathore2505@users.noreply.github.com>
2019-05-02 03:03:00 -0700

    Update TestCsvReader.groovy

--- vars/TestCsvReader.groovy
+++ vars/TestCsvReader.groovy
@@ -9,7 +9,7 @@ def call()
     List<String[]> allData = fileContents.split('\n')
     for (def i=0;i<=allData.size;i++) // Read All Lines
     {
-        boolean fruitFound = allData.any{i.contains('Emilconfig')}
+        boolean fruitFound = allData.any{i.toString().contains('Emilconfig')}
         if(fruitFound)
         {
            println '' + fruitFound + '.'  
