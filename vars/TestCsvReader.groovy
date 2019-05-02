4ab5be8b52fe38d15a723b801ebf49cc09da8a3c
Rathore2505 <49403036+Rathore2505@users.noreply.github.com>
2019-05-02 03:41:09 -0700

    Update TestCsvReader.groovy

--- vars/TestCsvReader.groovy
+++ vars/TestCsvReader.groovy
@@ -13,13 +13,12 @@ def call()
         if(allData[i].toString().contains('admin@merge.com'))
         {
           List<String[]> parts =   allData[i].split(',')
-            println '' + parts.size + '.'  
-            println '' + parts[1].toString() + '.'
+             println '' + parts[0].toString() + '.'
+             println '' + parts[1].toString() + '.'
              println '' + parts[2].toString() + '.'
-             println '' + parts[3].toString() + '.'
-             for(def j=0;j<=parts.size;j++)
+             for(def j=0;j<parts.size;j++)
               {
-                tmpMap.putAt(parts[j].toString())
+                tmpMap.putAt(parts[j])
               }
         }
     }
