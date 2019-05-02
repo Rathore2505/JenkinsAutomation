8a662201abe7bc6e21bafcab2e989b4bce823a5c
Rathore2505 <49403036+Rathore2505@users.noreply.github.com>
2019-05-02 02:16:48 -0700

    Update TestCsvReader.groovy

--- vars/TestCsvReader.groovy
+++ vars/TestCsvReader.groovy
@@ -6,7 +6,12 @@ def call()
 
     File csvFile = new File('C:\\JenkinsAutomation\\Newjob.csv')
     String fileContents = new File('C:/JenkinsAutomation/Newjob.csv').text
-    String[] lines = fileContents.split('\n')
+   List<String[]> lines = fileContents.split('\n')
+    //for (def i=0;i<lines.size;i++) // specify how many line need to read eg.. 30
+   // {
+    //log.info file1.readLines().get(i)  
+
+   // }
     println 'File All Text' + lines + '.'
     println 'File All Text' + fileContents + '.'
 
