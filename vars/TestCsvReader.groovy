4089de839c71290bc27e923e2773db879910b63b
Rathore2505 <49403036+Rathore2505@users.noreply.github.com>
2019-05-02 00:30:41 -0700

    Update TestCsvReader.groovy

--- vars/TestCsvReader.groovy
+++ vars/TestCsvReader.groovy
@@ -4,8 +4,10 @@ import com.xlson.groovycsv.CsvParser
 def call()
 {
 
-File csvFile = new File('C:\\JenkinsAutomation\\Newjob.csv')
-String fileContents = new File('C:/JenkinsAutomation/Newjob.csv').text
-    println 'Sending mail to ' + fileContents + '.'
+    File csvFile = new File('C:\\JenkinsAutomation\\Newjob.csv')
+    String fileContents = new File('C:/JenkinsAutomation/Newjob.csv').text
+    String[] lines = fileContents.text.split('\n')
+    println 'File All Text' + lines + '.'
+    println 'File All Text' + fileContents + '.'
 
 } 
