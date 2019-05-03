731c7a214217152f09ae7ca7eb04f3f84409961e
Rathore2505 <49403036+Rathore2505@users.noreply.github.com>
2019-05-03 05:42:32 -0700

    Update TestCsvReader.groovy

--- vars/TestCsvReader.groovy
+++ vars/TestCsvReader.groovy
@@ -4,22 +4,25 @@ import com.xlson.groovycsv.CsvParser
 def call(String Variable ,  int index)
 {
     
-    def list= []
+    //def list= []
     File csvFile = new File('C:\\JenkinsAutomation\\Newjob.csv')
-    String fileContents = new File('C:/JenkinsAutomation/Newjob.csv').text
-    List<String[]> allData = fileContents.split('\n')
-    for (def i=0;i<=allData.size;i++) // Read All Lines
-    {
-        if(allData[i].toString().contains('Variable'))
-        {
-          List<String[]> Spl = allData[i].split('=') 
-          List<String[]> parts =   Spl[1].split(',')
-          for(def j=0;j<parts.size;j++)
-            {
-            list.add(parts[j].toString()) 
-            }
-        }
-    }
-    return list[index]
+    //String fileContents = new File('C:/JenkinsAutomation/Newjob.csv').text
+    //List<String[]> allData = fileContents.split('\n')
+    //for (def i=0;i<=allData.size;i++) // Read All Lines
+    //{
+        //if(allData[i].toString().contains('Variable'))
+        //{
+         // List<String[]> Spl = allData[i].split('=') 
+          //List<String[]> parts =   Spl[1].split(',')
+          //for(def j=0;j<parts.size;j++)
+            //{
+            //list.add(parts[j].toString()) 
+            //}
+        //}
+    //}
+    //return list[index]
+    require 'csv'
+    csv = CSV.read(csvFile, :headers=>true)
+    p csv['name'] 
  
 } 
