bb6f5669de7cdd6d4732bb1a222384e52b748c36
Rathore2505 <49403036+Rathore2505@users.noreply.github.com>
2019-05-05 22:35:32 -0700

    Update TestCsvReader.groovy

--- vars/TestCsvReader.groovy
+++ vars/TestCsvReader.groovy
@@ -4,39 +4,25 @@ import com.xlson.groovycsv.CsvParser
 import au.com.bytecode.opencsv.CSVReader
 import au.com.bytecode.opencsv.bean.CsvToBean;
 import au.com.bytecode.opencsv.bean.HeaderColumnNameMappingStrategy;
-//def call(String Variable ,  int index)
-class Person {
-    String name
-    Integer age
-}
-def call()
+def call(String Variable ,  int index)
 {
     
-    //def list= []
+    def list= []
     File csvFile = new File('C:\\JenkinsAutomation\\Newjob.csv')
-    //String fileContents = new File('C:/JenkinsAutomation/Newjob.csv').text
-    //List<String[]> allData = fileContents.split('\n')
-    //for (def i=0;i<=allData.size;i++) // Read All Lines
-    //{
-        //if(allData[i].toString().contains('Variable'))
-        //{
-         // List<String[]> Spl = allData[i].split('=') 
-          //List<String[]> parts =   Spl[1].split(',')
-          //for(def j=0;j<parts.size;j++)
-            //{
-            //list.add(parts[j].toString()) 
-            //}
-        //}
-    //}
-    //return list[index]
-   def csv = """\
-name,age
-Charlie,23
-Billy,64"""
-
-// use the mapper
-def ctb = new CsvToBean<Person>()
-def hcnms = new HeaderColumnNameMappingStrategy<Person>()
-hcnms.type = Person
-println ctb.parse(hcnms, new StringReader(csv))
+    String fileContents = new File('C:/JenkinsAutomation/Newjob.csv').text
+    List<String[]> allData = fileContents.split('\n')
+    for (def i=0;i<=allData.size;i++) // Read All Lines
+    {
+        if(allData[i].toString().contains('Variable'))
+        {
+          List<String[]> Spl = allData[i].split('=') 
+          List<String[]> parts =   Spl[1].split(',')
+          for(def j=0;j<parts.size;j++)
+            {
+            list.add(parts[j].toString()) 
+            }
+        }
+    }
+    return list[index]
+ 
 } 
